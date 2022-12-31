db7183ed79668fa34c9c674f5ddcc34006a035b1
Maor97 <53435052+Maor97@users.noreply.github.com>
2023-01-01 00:34:19 +0200

    Add files via upload

---
+++ SetUp-ITPCs.ps1
@@ -0,0 +1,16 @@
+﻿#Change all the network card to private
+$Return = Set-NetConnectionProfile -Name * -NetworkCategory Private
+#Ebable PS Remoting
+$Return = Enable-PSRemoting –force
+#Change WinRM service start mode to automatic
+$Return = Start-Job -ScriptBlock{cmd /c "sc config WinRM start=auto"}
+#Enable WinRM in local firewall
+$Return = netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in action=allow service=any enable=yes profile=any localport=5985 protocol=tcp
+if ( $((Get-WMIObject win32_operatingsystem).name) -match "Microsoft Windows Server 2016*" ) {
+    $Return = New-NetFirewallRule -Direction Inbound -DisplayName "mDNS (UDP-In)" -Group "mDNS" -Profile Private -Enabled True -Action Allow -Program "%SystemRoot%\system32\svchost.exe" -RemoteAddress "LocalSubnet" -Protocol "UDP" -LocalPort "5353"
+}
+#Disable limit blank password use
+$Return = Set-ItemProperty -Path 'REGISTRY::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa' -Name 'LimitBlankPasswordUse' -Value 0 -force
+
+Add-Type -AssemblyName PresentationFramework
+$Return = [System.Windows.MessageBox]::Show('The computer is setup and ready for remote restart\stop. :-)','Success')
